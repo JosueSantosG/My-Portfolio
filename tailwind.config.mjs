d181274741ee4d66bb834fff379bcb2327e8eec4
JosueSantosG <josue.santosgonzabay@upse.edu.ec>
2024-06-21 23:18:47 -0500

    Updated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,20 +3,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {
-			fontFamily: {
-				inter: ['Inter', 'sans-serif'],
-			},
-			animation: {
-				'infinite-scroll': 'infinite-scroll 20s linear infinite',
-			},
-			keyframes: {
-				'infinite-scroll': {
-					from: { transform: 'translateX(0)' },
-					to: { transform: 'translateX(-50%)' },
-				}
-			}
-		},
+
 	},
 	plugins: [],
 }
